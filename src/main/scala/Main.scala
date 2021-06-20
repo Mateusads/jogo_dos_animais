02d76ae8c1303ca82c9e37c42db46a220dc64b40
Mateus <mateusowmedeiros@gmail.com>
2021-06-20 11:03:09 -0300

    case class question, win, learn

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,30 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import scala.io.StdIn.readLine
 
-def msg = "I was compiled by Scala 3. :)"
+trait Answer(question: String, yes: Option[Answer] = None, no: Option[Answer] = None)
+
+case class Yes(question: String, yes: Option[Answer] = None, no: Option[Answer] = None) extends Answer(question, yes, no)
+
+case class No(question: String, yes: Option[Answer] = None, no: Option[Answer] = None) extends Answer(question, yes , no)
+
+case class Question(question: String, yes: Option[Answer], no: Option[Answer]) extends Answer(question, yes, no)
+
+case class LearnAnimal() extends Answer (question = "Qual animal você pensou")
+
+case class LearnAbility() extends Answer (question = "O que o animal faz, que um animalAntigo não")
+
+case class Win() extends Answer (question = "Ganhei novamente")
+
+
+@main def hello: Unit = {
+  while
+    val line = readLine()
+    println(s"Read: $line")
+    line != ""
+  do ()
+
+  println("Hello world!")
+  
+}
+
+
+
